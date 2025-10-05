f586a364c48929d955f8ba6183f90e6ececd42d9
JosephAntony37900 <joseantoniopintoaguilar@gmail.com>
2025-10-04 22:22:06 -0600

    condiciones de carrera solucionado con lockfile

--- src/ActionsRegistry/services/actionsRegistryService.js
+++ src/ActionsRegistry/services/actionsRegistryService.js
@@ -1,24 +1,25 @@
 import { promises as fs } from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import lockfile from 'proper-lockfile';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const filePath = path.join(__dirname, '../db/actionsRegistry.json');
+const filePath = path.join(__dirname, '../../../db/actionsRegistry.json');
 
 class ActionsRegistryService {
     static instance;
 
     constructor() {}
 
-    static getInstance(){
-        if(!ActionsRegistryService.instance){
+    static getInstance() {
+        if (!ActionsRegistryService.instance) {
             ActionsRegistryService.instance = new ActionsRegistryService();
         }
         return ActionsRegistryService.instance;
     }
 
-    async getActions(){
+    async getActions() {
         try {
             const content = await fs.readFile(filePath, 'utf-8');
             return JSON.parse(content || '[]');
@@ -31,9 +32,16 @@ class ActionsRegistryService {
     }
 
     async addAction(action) {
-        const allActions = await this.getActions();
-        allActions.push(action);
-        await fs.writeFile(filePath, JSON.stringify(allActions, null, 2));
+        try {
+            await lockfile.lock(filePath, { retries: { retries: 5, factor: 1.2, minTimeout: 100 } });
+            
+            const allActions = await this.getActions();
+            allActions.push(action);
+            await fs.writeFile(filePath, JSON.stringify(allActions, null, 2));
+            
+        } finally {
+            await lockfile.unlock(filePath);
+        }
     }
 }
 
